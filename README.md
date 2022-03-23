4ae7f44aef16c76e02e516f2922cd988e72209f3
menglycheng <cheng.mengly19@kti.edu.kh>
2022-03-23 23:02:39 +0700

    v 0.0.1

---
+++ README.md
@@ -0,0 +1,12 @@
+# dodo
+it is create odoo module cli 
+
+## Installation
+```pip install dodo```
+
+
+## License
+
+© 2022 Mengly
+
+This repository is licensed under the MIT license. See LICENSE for details.
--- setup.py
+++ setup.py
@@ -5,15 +5,25 @@ def read_requirments():
         content = req.read()
         requirements = content.split('\n')
     return requirements
-
+DESCRIPTION = ''
 setup(
     name = 'dodo',
-     version = '0.1.0',
+    version = '0.0.1',
+    author= "mengly",
+    author_email='menglycheng2@gmail.com',
     packages = find_packages(),
+    description= DESCRIPTION,
     include_package_data=True,
-    install_requires = read_requirments()
-    entry_points = {
+    install_requires = ["click==7.1.2"],
+    keywords= ['odoo', 'odoo module', 'create odoo module', ''],
+    classifiers= [
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix'
+    ],
+     entry_points = {
         'console_scripts': [
             'dodo = dodo.__main__:main'
         ]
-    })
+    }
+ )
